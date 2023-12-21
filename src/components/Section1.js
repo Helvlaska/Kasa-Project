d68e66e2275a900ca53c3186dc7f8c9e62eaed21
Claire Castan <ccastan@live.fr>
2023-12-21 11:24:44 +0100

    :sparkles: (compo/Section1 + styles/Section1) Ajouts des fichiers Section1.js et Section1.scss

---
+++ src/components/Section1.js
@@ -0,0 +1,10 @@
+/****IMPORTATIONS****/
+import '../styles/Section1.scss'
+import Section1_img from '../assets/Section1_img.png'
+function Section1(){
+    return <div className='Kasa_Section1'>
+        <img src={Section1_img} alt='Kasa' className='Kasa_Section1_img' />
+    </div>
+}
+
+export default Section1
