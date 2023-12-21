c0e2c55bbd3ee8119661709cbf92111dd8c51cd3
Claire Castan <ccastan@live.fr>
2023-12-21 08:53:34 +0100

    :sparkles: (assets/Logo) Ajout de l'image Logo, intégration dans le Header, ajout du style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 /****IMPORTATIONS****/
 import '../styles/Header.scss'
+import Logo from '../assets/Logo.svg'
 
 function Header(){
     return <div className='Kasa_Header'>
-
+        <img src={Logo} alt='Kasa' className='Kasa_Logo' />
     </div>
 }
 
