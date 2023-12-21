94511421dc31c832461280137f777f73c7bef975
Claire Castan <ccastan@live.fr>
2023-12-21 09:20:48 +0100

    :seedling:(Nav.js Nav.scss) Mise en place du code et du style de l'élément Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 /****IMPORTATIONS****/
 import '../styles/Header.scss'
 import Logo from '../assets/Logo.svg'
+import Nav from './Nav'
 
 function Header(){
     return <div className='Kasa_Header'>
         <img src={Logo} alt='Kasa' className='Kasa_Logo' />
+        <Nav />
     </div>
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+/****IMPORTATIONS****/
+import '../styles/Nav.scss'
+
+function Nav(){
+    return <div className='Kasa_Nav'>
+        <p className='Kasa_Nav_Txt'>Accueil</p>
+        <p className='Kasa_Nav_Txt'>A Propos</p>
+    </div>
+}
+
+export default Nav
