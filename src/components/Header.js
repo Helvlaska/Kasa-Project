18c35846bb3dbfaa4cccfed104a1ec2095b53c15
Claire Castan <ccastan@live.fr>
2023-12-21 08:45:38 +0100

    :seedling:(Header.js Header.scss) Mise en place du code et du style de l'élément Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+/****IMPORTATIONS****/
+import '../styles/Header.scss'
+
+function Header(){
+    return <div className='Kasa_Header'>
+
+    </div>
+}
+
+export default Header
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 /****IMPORTATIONS****/
 import '../styles/Home.scss'
+import Header from './Header'
 
 function Home(){
     return <div className='Kasa_Home'>
-
+        <Header />
     </div>
 }
 
