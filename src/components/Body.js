ed28337035912ac8ba942a4c78b790f5456c4619
Claire Castan <ccastan@live.fr>
2023-12-21 10:00:40 +0100

    :sparkles: (compo/Gallery + styles/Gallery) Ajouts des fichiers Gallery.js et Gallery.scss

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,10 @@
 /****IMPORTATIONS****/
 import '../styles/Body.scss'
+import Gallery from './Gallery'
 
 function Body(){
     return <div className='Kasa_Body'>
-
+        <Gallery />
     </div>
 }
 
---
+++ src/components/Gallery.js
@@ -0,0 +1,10 @@
+/****IMPORTATIONS****/
+import '../styles/Gallery.scss'
+
+function Gallery(){
+    return <div className='Kasa_Gallery'>
+
+    </div>
+}
+
+export default Gallery
