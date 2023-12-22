3ed62f42f76af653e0c9163a65ac7a900eefa857
Claire Castan <ccastan@live.fr>
2023-12-22 08:49:52 +0100

    :seedling: (Section1.js + Section1.scss) Ajout d'un filtre et de texte sur l'image

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -4,6 +4,8 @@ import Section1_img from '../assets/Section1_img.png'
 function Section1(){
     return <div className='Kasa_Section1'>
         <img src={Section1_img} alt='Kasa' className='Kasa_Section1_img' />
+        <div className='Kasa_Section1_Filter'></div>
+        <p className='Kasa_Section1_Txt'>Chez vous, partout et ailleurs</p>
     </div>
 }
 
