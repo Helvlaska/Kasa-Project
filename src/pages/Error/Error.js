087e4e61d2a2cae6ece0fc2545cacca299ff7b96
Claire Castan <ccastan@live.fr>
2024-02-17 13:57:29 +0100

    :seedling: (src/pages/Error) Ajout d'une className au body et modification du css du link

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Error() {
     return (
-        <div>
+        <div className='Kaza_Error'>
             <h1 className='Error_Title'>404</h1>
             <p className='Error_Content'>Oups! La page que vous demandez n'existe pas.</p>
             <Link className='Error_Link' to="/">Retourner sur la page d'accueil</Link>
