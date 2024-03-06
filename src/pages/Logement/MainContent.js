602877fc82fd44ff0028c0a1e5ea2fa285544b74
Claire Castan <ccastan@live.fr>
2024-03-06 16:07:45 +0100

    :sparkles: (src/Logement/MainContent) Ajout de commentaires sur la function MainContent

--- src/pages/Logement/MainContent.js
+++ src/pages/Logement/MainContent.js
@@ -1,66 +1,70 @@
 import React, { useEffect, useState } from 'react'; // Importation des hooks React nécessaires
-import { useParams } from 'react-router-dom';
-import Api from '../ApiLogements.json'
-import Arrow from '../../assets/Arrow.svg'
-import '../../styles/DropDown.scss'
+import { useParams } from 'react-router-dom'; // Importation de useParams pour récupérer les paramètres d'URL
+import Api from '../ApiLogements.json' // Importation des données d'API
+import Arrow from '../../assets/Arrow.svg' // Importation de l'icône flèche
+import '../../styles/DropDown.scss' // Importation des styles CSS
 
 function MainContent(){
 
-    const { identifiant } = useParams(); // Récupère l'identifiant de l'URL
+    const { identifiant } = useParams(); // Récupération de l'identifiant de l'URL
     const [MainContentData, setMainContentData] = useState(null); // État pour stocker les données du logement
     
-    const [dropDown, setDropDown] = useState({}); // Ligne ajoutée
+    const [dropDown, setDropDown] = useState({}); // État pour gérer l'état des éléments déroulants
 
-    const startDropDown = (id) => {
-        setDropDown(prevState => ({ ...prevState, [id]: !prevState[id] }));
+    const startDropDown = (id) => { // Fonction pour ouvrir ou fermer un élément déroulant
+        setDropDown(prevState => ({ ...prevState, [id]: !prevState[id] })); // Utilisation de la fonction setDropDown pour mettre à jour l'état avec l'inversion de la visibilité de l'élément spécifique
     }
 
-    useEffect(() => {
-        // Recherchez les données correspondant à l'identifiant dans le tableau de données JSON
+    useEffect(() => { // Effet qui s'exécute au chargement et lorsque l'identifiant change
+        // Recherche des données correspondant à l'identifiant dans le tableau de données JSON
         const data = Api.find(item => item.identifiant === identifiant);
     
-        // Met à jour l'état avec les données trouvées
+        // Mise à jour de l'état avec les données trouvées
         setMainContentData(data);
-      }, [identifiant]);
+      }, [identifiant]); // Dépendance : identifiant de l'URL
     
-      if (!MainContentData) {
-        return <div>Loading...</div>;
+      if (!MainContentData) { // Si les données du logement ne sont pas chargées
+        return <div>Loading...</div>; // Affichage d'un message de chargement
       }
-    const TagList = (MainContentData["Mots clés"]);
-    const équipementsList = (MainContentData.équipements);
+    const TagList = (MainContentData["Mots clés"]); // Liste des mots-clés du logement
+    const équipementsList = (MainContentData.équipements); // Liste des équipements du logement
 
-    return <div className='Kasa_MainContent'>
-        <div className='MainContent_Bloc_Title'>
-            <h1 className="MainContent_Title">{MainContentData.title}</h1>
-            <p className="MainContent_Lieu">{MainContentData.location}</p>
-            <ul className="MainContent_TagList">
-                {TagList.map((item, Tag) => (
-                    <li key={`${item}-${Tag}`}className='MainContent_Tag'>{item}</li>
+    return <div className='Kasa_MainContent'> {/* Conteneur principal */}
+        <div className='MainContent_Bloc_Title'> {/* Bloc de titre */}
+            <h1 className="MainContent_Title">{MainContentData.title}</h1> {/* Affichage du titre du logement */}
+            <p className="MainContent_Lieu">{MainContentData.location}</p> {/* Affichage de l'emplacement du logement */}
+            <ul className="MainContent_TagList"> {/* Liste des mots-clés */}
+                {/* Parcours et affichage des mots-clés */}
+                {TagList.map((item, Tag) => ( 
+                    <li key={`${item}-${Tag}`}className='MainContent_Tag'>{item}</li> 
                 ))}
             </ul>
         </div>
-        <div className='MainContent_Bloc_People'>
+        <div className='MainContent_Bloc_People'> {/* Bloc pour les informations sur les personnes */}
 
         </div>
-        <div className='MainContent_Bloc_DropDown'>
-            <div className='DropDown_Item'>
-                <div className='DropDown_Title'> 
-                    <h6>Description</h6>
-                    <img src={Arrow} alt='Arrow' onClick={() => startDropDown('description')} /> {/*événement onClick qui appelle la fonction startDropDown avec l'index de l'élément en tant qu'argument*/}
+        <div className='MainContent_Bloc_DropDown'> {/* Bloc pour les éléments déroulants */}
+            <div className='DropDown_Item'> {/* Élément déroulant pour la description */}
+                <div className='DropDown_Title'> {/* Bloc titre de l'élément déroulant */}
+                    <h6>Description</h6> {/* Titre de la description */}
+                    <img src={Arrow} alt='Arrow' onClick={() => startDropDown('description')} /> {/* Icône de flèche avec événement onClick pour ouvrir/fermer l'élément */}
                 </div>
+                    {/* Vérification si l'élément déroulant de la description est ouvert */}
                     {dropDown['description'] && ( 
-                        <div> {/*Utilisation de l'état local pour déterminer si le contenu déroulant doit être affiché*/}
-                            <p className='DropDown_Text'>{MainContentData.description}</p>
+                        <div> {/* Contenu de l'élément déroulant */}
+                            <p className='DropDown_Text'>{MainContentData.description}</p> {/* Affichage de la description */}
                         </div>
                     )}   
                 </div>
-            <div className='DropDown_Item'>
-                <div className='DropDown_Title'> 
-                    <h6>Équipements</h6>
-                    <img src={Arrow} alt='Arrow' onClick={() => startDropDown('équipements')} /> {/*événement onClick qui appelle la fonction startDropDown avec l'index de l'élément en tant qu'argument*/}
+            <div className='DropDown_Item'> {/* Élément déroulant pour les équipements */}
+                <div className='DropDown_Title'> {/* Bloc titre de l'élément déroulant */}
+                    <h6>Équipements</h6> {/* Titre de la description */}
+                    <img src={Arrow} alt='Arrow' onClick={() => startDropDown('équipements')} /> {/* Icône de flèche avec événement onClick pour ouvrir/fermer l'élément */}
                 </div>
+                {/* Vérification si l'élément déroulant de la description est ouvert */}
                 {dropDown['équipements'] && ( 
-                    <ul className='DropDown_List'> {/*Utilisation de l'état local pour déterminer si le contenu déroulant doit être affiché*/}
+                    <ul className='DropDown_List'> {/* Liste des équipements */}
+                        {/* Parcours et affichage des équipements */}
                         {équipementsList.map((item, équipements) => (
                             <li key={`${item}-${équipements}`} className='DropDown_Text'>{item}</li>
                         ))}   
@@ -71,4 +75,4 @@ function MainContent(){
     </div>
 }
 
-export default MainContent
+export default MainContent // Exportation de la fonction MainContent
