903e60be13040c1bb13c01d0a06a8ec0df716d7b
Claire Castan <ccastan@live.fr>
2024-03-29 09:30:53 +0100

    :seedling::lipstick: (pages/Error/Error) Composant responsive desktop/mobile

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom'
 
 function Error() {
     return (
-        <div className='Kaza_Error'>
-            <h1 className='Error_Title'>404</h1>
-            <p className='Error_Content'>Oups! La page que vous demandez n'existe pas.</p>
+        <div className='Error'>
+            <div className='Error_Content'>
+                <h1 className='Content_Title'>404</h1>
+                <p className='Content_Content'>Oups! La page que vous demandez n'existe pas.</p>
+            </div>
             <Link className='Error_Link' to="/">Retourner sur la page d'accueil</Link>
         </div>
     )
