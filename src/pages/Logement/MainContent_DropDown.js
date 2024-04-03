2633677e56241e9102e2cd49455da174567da66b
Claire Castan <ccastan@live.fr>
2024-04-03 14:38:49 +0200

    :speech_balloon: (Pages/Logement/MainContent_DropDown.js) Ajout de commentaires

--- src/pages/Logement/MainContent_DropDown.js
+++ src/pages/Logement/MainContent_DropDown.js
@@ -1,16 +1,21 @@
 import { useMainContentData } from '../Data/MainContentData'; // Importation de la fonction d'appel a l'Api
-import {DropDown} from '../Componants/DropDown/DropDown' // Importation du composant générique DropDown
-import '../Componants/DropDown/DropDown.scss' // Importation du css du composant
+import {DropDown} from '../Componants/DropDown/DropDown' // Importation du composant réutilisable DropDown
+import '../Componants/DropDown/DropDown.scss' // Importation du style du composant
 
 function DropDownLogement() {
 
-    const MainContentData = useMainContentData(); // Utilisation de la fonction d'importation des données de l'Api
+    // Utilisation de la fonction d'importation des données de l'Api
+    const MainContentData = useMainContentData(); 
+
+    // vérification de nullité des données de l'Api
     if (!MainContentData) {
-        return <div>Loading...</div>; // vérification de nullité des données de l'Api
+        return <div>Loading...</div>; 
       }
 
     return (
         <div className='MainContent_DropDown'>
+            {/*Utilisation de la props : title et content déclaré dans la fonction réutilisable DropDown*/}
+            {/*Content prend comme valeur, celles des clefs description et équipements de l'api */}
             <DropDown title='Description' content={MainContentData.description}/>
             <DropDown title='Equipements' content={MainContentData.équipements}/>
         </div>)
