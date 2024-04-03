e1944f3e015a5781fb40e927e9c7473cb26853aa
Claire Castan <ccastan@live.fr>
2024-04-03 13:55:46 +0200

    :speech_balloon: (Pages/Componants/SectionImage/SectionImage.js) Ajout de commentaires

--- src/pages/Componants/SectionImg/SectionImg.js
+++ src/pages/Componants/SectionImg/SectionImg.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import './SectionImg.scss'
+import './SectionImg.scss' // Import du style du composant
 
+// Création d'une fonction qui prend comme props src et children = composant réutilisable
 function SectionImage({ src, children }) {
   
     return (<section className="SectionImg">
+      {/* la src de l'image prendra sa valeur dans la fonction de la page où elle sera déclarée */}
       <img src={src} alt="Section_Image" className='SectionImg_Img' />
+      {/* children permet dans cette fonction réutilisable d'ajouter (ou pas) une balise/composant */}
       {children}
     </section>
   );
