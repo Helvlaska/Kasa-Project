88fd6f0d86e33576cbaa2b20e0321d1a0254c1ab
Claire Castan <ccastan@live.fr>
2024-04-03 15:32:22 +0200

    :speech_balloon: (Pages/Propos/DropDown.js) Ajout de commentaires

--- src/pages/Propos/DropDown.js
+++ src/pages/Propos/DropDown.js
@@ -1,13 +1,15 @@
 import Api from './ApiContent.json' // Importation du fichier de données
-import {DropDown} from '../Componants/DropDown/DropDown' // Importation du composant générique DropDown
-import '../Componants/DropDown/DropDown.scss' // Importation du css du composant
+import {DropDown} from '../Componants/DropDown/DropDown' // Importation du composant réutilisable DropDown
+import '../Componants/DropDown/DropDown.scss' // Importation du style du composant
 
 function DropDownPropos() {
 
     return (
-        <ul className='DropDown_List'> {/*Utilisation de la fonction map pour parcourir chaque élément de la liste*/}
+        <ul className='DropDown_List'> 
+            {/*Crée une liste avec les données de l'api*/}
             {Api.map((item, id) => (
                 <li key={id} className='DropDown_Item_Propos'>
+                    {/*On donne une valeur au props déclarés dans le composant réutilisable DropDown*/}
                     <DropDown title={item.Title} content={item.Content}/>   
                 </li>   
             ))}
