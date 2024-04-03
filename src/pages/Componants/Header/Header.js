9186864e09d9b7291d5ae3e557d03cdd7e018ffe
Claire Castan <ccastan@live.fr>
2024-04-03 11:52:52 +0200

    :speech_balloon: (Pages/Componants/Header/Header.js) Ajout de commentaires

--- src/pages/Componants/Header/Header.js
+++ src/pages/Componants/Header/Header.js
@@ -1,16 +1,15 @@
-/****IMPORTATIONS****/
-import './Header.scss'
-import Logo from '../../../assets/LogoPink.svg'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom' // Importation de routes via des link
+import Logo from '../../../assets/LogoPink.svg' // Importation de l'image de Logo
+import './Header.scss' // Importation du style du composant
 
 function Header(){
     return <div className='Header'>
-        <Link className='Header_Logo' to="/">
+        <Link className='Header_Logo' to="/"> {/* Le link renvoie à la page Home (accueil) */}
             <img src={Logo} alt='Kasa'/>
         </Link>        
         <nav className='Header_Nav'>
-            <Link className='Nav_Content' to="/">Accueil</Link>
-            <Link className='Nav_Content' to="/Propos">À Propos</Link>
+            <Link className='Nav_Content' to="/">Accueil</Link> {/* Le link renvoie à la page Home (accueil) */}
+            <Link className='Nav_Content' to="/Propos">À Propos</Link> {/* Le link renvoie à la page Propos (à Propos) */}
         </nav>
     </div>
 }
