d2ce030e7d2f5f6712785013481e1ea162c8e25d
Claire Castan <ccastan@live.fr>
2024-04-03 11:42:10 +0200

    :speech_balloon: (Pages/Componants/DropDown/DropDown.js) Ajout de commentaires

--- src/pages/Componants/DropDown/DropDown.js
+++ src/pages/Componants/DropDown/DropDown.js
@@ -1,30 +1,40 @@
-import React, { useState } from 'react' // Import du Hook useState pour gérer l'état local du componant
-import Arrow from '../../../assets/Arrow.svg' // Import de l'image de fleche
-import './DropDown.scss'
+import React, { useState } from 'react' // Importation du Hook useState
+import Arrow from '../../../assets/Arrow.svg' // Importation de l'image de flèche
+import './DropDown.scss' // Importation du style du DropDown
 
-export function DropDown({ title, content }) { // la fonction prend deux props (title, content) en propriétés
+// la fonction prend deux props (title, content) en propriétés
+export function DropDown({ title, content }) { 
 
-  const [dropDown, setDropDown] = useState(false); // État pour gérer l'état de l'élément déroulant
+  // État pour gérer l'état de l'élément déroulant : dropDown vaut false par défaut
+  const [dropDown, setDropDown] = useState(false); 
 
-  const classNameAnimation = dropDown ? 'open' : 'closed'; // Variable pour créer de nouvelles class si le drop down est ouvert ou fermé
+  // Fonction conditionnelle : si dropDown ouvert = Appliquer la className 'open' si fermé = className 'closed'
+  const classNameAnimation = dropDown ? 'open' : 'closed'; 
   
   const startDropDown = () => { // Fonction pour ouvrir ou fermer l'élément déroulant
     setDropDown(prevState => !prevState); // Inversion de l'état actuel
   };
 
+  // La fonction va retourner une div avec les paramètres des const établies plus haut
   return (
     <div className='DropDown DropDown_Item_Logement DropDown_Item_Propos' >
+      {/* Création d'une div avec une className fixe et dynamique selon si le dropDown est ouvert ou fermé, avec un écouteur d'évènement */}
       <div className={`DropDown_Title ${classNameAnimation}`} onClick={startDropDown} >
-        <h6>{title}</h6>
+        <h6>{title}</h6> {/* Donnée title importée via les props */}
         <img src={Arrow} alt='Arrow' className='Arrow' />
       </div>
+      {/* Création d'une div avec une className fixe et dynamique selon si le dropDown est ouvert ou fermé */}
       <div className={`DropDown_Content ${classNameAnimation}`} > 
+        {/* Condition si le dropDown est ouvert, renvoyer ...*/}
         {dropDown && (
           <>
-            {typeof content === 'string' ? ( // Ajout d'une condition pour déterminer le type de contenu de la donnée (paragraphe, liste,...)
+           {/* Ajout d'une condition pour déterminer le type de contenu de la donnée (paragraphe, liste,...)*/}
+            {typeof content === 'string' ? ( 
+              // si c'est du type string renvoyer un <p>
               <p className='Content'>{content}</p>
-            ) : (
+            ) : ( // sinon renvoyer les données sous forme de liste
               <ul className='Content_List'>
+                {/* on génère une liste (.map) via le tableau de données (content) */}
                 {content.map((item, index) => (
                   <li key={index}>{item}</li>
                 ))}
