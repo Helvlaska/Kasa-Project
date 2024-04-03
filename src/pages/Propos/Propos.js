715b2f620014cdc826a716ea2ada89768d4486dc
Claire Castan <ccastan@live.fr>
2024-04-03 15:37:47 +0200

    :speech_balloon: (Pages/Propos/Propos.js) Ajout de commentaires

--- src/pages/Propos/Propos.js
+++ src/pages/Propos/Propos.js
@@ -1,11 +1,12 @@
-import './Propos.scss'
-import SectionImage from '../Componants/SectionImg/SectionImg'
-import Image from '../../assets/SectionImg_Propos.png' 
-import DropDown from './DropDown'
+import Image from '../../assets/SectionImg_Propos.png' // Importation de l'image pour SectionImage
+import SectionImage from '../Componants/SectionImg/SectionImg' // Importation du composant réutilisable SectionImage
+import DropDown from './DropDown' // Importation du composant DropDown
+import './Propos.scss' // Importation du style de la page
 
 function Propos(){
     return (
         <div className='Propos'>
+            {/*Ajout de la valeur de la props déclarée dans le composant réutilisable SectionImage*/}
             <SectionImage src={Image}/>
             <DropDown />
         </div>
