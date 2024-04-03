7bbba2aed0f4160d9f9984ac992afc0b66590eb2
Claire Castan <ccastan@live.fr>
2024-04-03 14:03:20 +0200

    :speech_balloon: (Pages/Data/MainContentData.js) Ajout de commentaires

--- src/pages/Data/MainContentData.js
+++ src/pages/Data/MainContentData.js
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react'; // Importation des Hook UseEffect e
 import { useParams, useNavigate } from 'react-router-dom'; // Importation de useParams et useNavigate
 import Api from './ApiLogements.json'; // Importation fichier Api
 
-export function useMainContentData() { // Rend la fonction useMainContentData exportable
+export function useMainContentData() { // Rend la fonction useMainContentData exportable donc réutilisable
   const { identifiant } = useParams(); // Hook useParams pour l'utilisation de paramètre spécifique
   const navigate = useNavigate(); // Hook useNavigate pour la redirection
-  const [MainContentData, setMainContentData] = useState(null);
+  const [MainContentData, setMainContentData] = useState(null); // Passe l'état de MainContentData à null, et modifiable avec setMainContentData
 
   useEffect(() => {
     // Recherche des données correspondant à l'identifiant dans le tableau de données JSON
@@ -15,10 +15,10 @@ export function useMainContentData() { // Rend la fonction useMainContentData ex
       // Si les données ne sont pas trouvées, rediriger vers la page 404
       navigate('/404');
     } else {
-      // Mise à jour de l'état avec les données trouvées
+      // Sinon : Mise à jour de l'état avec les données trouvées
       setMainContentData(data);
     }
-  }, [identifiant, navigate]); // Dépendances : identifiant de l'URL et la fonction de navigation
+  }, [identifiant, navigate]); // Crée un tableau avec l'identifiant récupéré ou la redirection en cas d'echec
 
-  return MainContentData;
+  return MainContentData; // Retourne MainContentData avec son changement d'état
 }
