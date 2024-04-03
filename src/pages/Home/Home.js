63f884f35d7e3c92192757e4c0684e096b2a2a06
Claire Castan <ccastan@live.fr>
2024-04-03 14:26:54 +0200

    :speech_balloon: (Pages/Home/Home.js) Ajout de commentaires

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,13 @@
-// fichier de construction de pages
-
-/****IMPORTATIONS****/
-import './Home.scss'
-import SectionImage from '../Componants/SectionImg/SectionImg'
-import Image from '../../assets/SectionImg_Accueil.png' 
-import Gallery from './Gallery'
+import Image from '../../assets/SectionImg_Accueil.png' // Import de l'image pour la SectionImage
+import SectionImage from '../Componants/SectionImg/SectionImg' // Import du composant SectionImage
+import Gallery from './Gallery' // Import du composant Gallery
+import './Home.scss' // Import du style de la page
 
 function Home(){
     return <div className='Home'>
-        <SectionImage src={Image}>
-            <p className='SectionImg_Content'>Chez vous, partout et ailleurs</p>
+        <SectionImage src={Image}> {/*src : props déclaré dans la fonction SectionImage*/}
+            {/*children : props déclaré dans la fonction SectionImage*/}
+            <p className='SectionImg_Content'>Chez vous, partout et ailleurs</p> 
         </SectionImage>
         <Gallery />
     </div>
